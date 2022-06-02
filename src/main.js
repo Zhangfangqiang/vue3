4fa4cc831d2a52a43a6b18fe2d28f074de8094dc
六指黑侠 <zf18600004319@aliyun.com>
2022-06-02 19:35:14 +0800

    台式电脑王超

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue' //引入 vue.js 创建方法
+import App from './App.vue'   //引入 App.vue 根视图
 
-createApp(App).mount('#app')
+
+
+
+createApp(App).mount('#app')  //创建一个新的 Vue 实例
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/vuejs-essential/dist/' : '/'    //判断环境是否是生产环境如果是, 就在./dist目录中创建文件 这样我们在使用 GitHub Pages 时，才能正确地引用静态资源。如果你不需要 GitHub Pages 了，可以将其去掉。
 })
